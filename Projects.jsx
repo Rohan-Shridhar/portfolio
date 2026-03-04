7faac2d63d56740e72bf51d9090f3cba65dda837
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-04 18:37:15 +0530

    Changed position of github repo button closer to view demo button

--- Projects.jsx
+++ Projects.jsx
@@ -15,8 +15,8 @@ const Projects = () => {
                     <div className="project-card-title">GridCraft</div>
                     <div className="project-card-desc">Front end application using react.js that runs directly in your browser used to create pixel arts in a 16 X 16 grid and download as an image</div>
                     <div className="project-card-btn">
-                        <a href="https://github.com/Rohan-Shridhar/gridcraft"><i className="fab fa-github"></i></a>
                         <div />
+                        <a href="https://github.com/Rohan-Shridhar/gridcraft"><i className="fab fa-github"></i></a>
                         <a href="https://rohan-shridhar.github.io/gridcraft/"><i className="fas fa-globe"></i></a>
                     </div>
                 </div>
@@ -27,8 +27,8 @@ const Projects = () => {
                     <div className="project-card-title">Prisoners Delima</div>
                     <div className="project-card-desc">The Prisoner's Dilemma is a classic thought experiment in game theory, a field that studies how and why people make decisions when their outcomes depend on the choices of others.</div>
                     <div className="project-card-btn">
-                        <a href="https://github.com/Rohan-Shridhar/Prisoners-Delima"><i className="fab fa-github"></i></a>
                         <div />
+                        <a href="https://github.com/Rohan-Shridhar/Prisoners-Delima"><i className="fab fa-github"></i></a>
                         <a href="https://rohan-shridhar.github.io/Prisoners-Delima/"><i className="fas fa-globe"></i></a>
                     </div>
                 </div>
@@ -39,8 +39,8 @@ const Projects = () => {
                     <div className="project-card-title">Attendance Tracker</div>
                     <div className="project-card-desc">A web application that tracks attendance of students and lets teachers mark attendance of students.</div>
                     <div className="project-card-btn">
-                        <a href="https://github.com/Rohan-Shridhar/Attendance-Tracker"><i className="fab fa-github"></i></a>
                         <div />
+                        <a href="https://github.com/Rohan-Shridhar/Attendance-Tracker"><i className="fab fa-github"></i></a>
                         <a href="https://attendance-tracker-seven-kappa.vercel.app/"><i className="fas fa-globe"></i></a>
                     </div>
                 </div>
