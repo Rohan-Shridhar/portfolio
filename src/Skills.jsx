49b9a256ff823d165c23c1be9a9a05d84f410d98
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-05 22:25:02 +0530

    Introduced new theme and tags for skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -5,7 +5,7 @@ const react = "images/Web/react.png"
 const python = "images/languages/python.png"
 const cpp = "images/languages/cpp.png"
 const java = "images/languages/java.png"
-const sql = "images/DBs/postgress.png"
+const sql = "images/DBs/mysql.png"
 const md = "images/Tools/markdown.png" // Corrected 'tools' to 'Tools'
 const mongodb = "images/DBs/mongodb.png"
 const git = "images/Tools/git.png"
@@ -19,34 +19,33 @@ const Skills = () => {
         <div className="skills-cont" id="skills">
             <div className="skills-title">What do I know ?</div>
             <div className="skill-row-1">
-                <div className="skill-js" id="skill"><img src={js} alt="js" /></div>
-                <div className="skill-java" id="skill"><img src={java} alt="java" /></div>
-                <div className="skill-py" id="skill"><img src={python} alt="python" /></div>
-                <div className="skill-cpp" id="skill"><img src={cpp} alt="cpp" /></div>
+                <div className="skill-js" id="skill"><img src={js} alt="js" /><br /><hr />Intermediate</div>
+                <div className="skill-java" id="skill"><img src={java} alt="java" /><br /><hr />Intermediate</div>
+                <div className="skill-py" id="skill"><img src={python} alt="python" /><br /><hr />Beginner</div>
+                <div className="skill-cpp" id="skill"><img src={cpp} alt="cpp" /><br /><hr />Intermediate</div>
                 <div></div>
                 <p className="lang">Languages</p>
             </div>
             <div className="skill-row-2">
-                <div className="skill-html" id="skill"><img src={html} alt="html" /></div>
-                <div className="skill-css" id="skill"><img src={css} alt="css" /></div>
-                <div className="skill-react" id="skill"><img src={react} alt="react" /></div>
+                <div className="skill-html" id="skill"><img src={html} alt="html" /><br /><hr />Intermediate</div>
+                <div className="skill-css" id="skill"><img src={css} alt="css" /><br /><hr />Intermediate</div>
+                <div className="skill-react" id="skill"><img src={react} alt="react" /><br /><hr />Intermediate</div>
                 <div></div>
                 <p className="lang">Web Dev</p>
             </div>
             <div className="skill-row-3">
-                <div className="skill-sql" id="skill"><img src={sql} alt="PostgresSql" /></div>
-                <div className="skill-mongodb" id="skill"><img src={mongodb} alt="mongodb" /></div>
+                <div className="skill-sql" id="skill"><img src={sql} alt="PostgresSql" /><br /><hr />Intermediate</div>
+                <div className="skill-mongodb" id="skill"><img src={mongodb} alt="mongodb" /><br /><hr />Beginner</div>
                 <div></div>
                 <p className="lang">Databases</p>
             </div>
             <div className="skill-row-4">
-                <div className="skill-git" id="skill"><img src={git} alt="Git version control" /></div>
-                <div className="skills-vscode" id="skill"><img src={vscode} alt="VS Code" /></div>
-                <div className="skills-antigravity" id="skill"><img src={antigravity} alt="Antigravity" /></div>
-                <div className="skill-md" id="skill"><img src={md} alt="Markdown" /></div>
-                <div className="skills-figma" id="skill"><img src={figma} alt="Figma" /></div>
-                <div className="skills-vercel" id="skill"><img src={vercel} alt="Vercel" /></div>
-                <div></div>
+                <div className="skill-git" id="skill"><img src={git} alt="Git version control" /><br /><hr />Version Control</div>
+                <div className="skills-vscode" id="skill"><img src={vscode} alt="VS Code" /><br /><hr />Editor</div>
+                <div className="skills-antigravity" id="skill"><img src={antigravity} alt="Antigravity" /><br /><hr />Editor</div>
+                <div className="skill-md" id="skill"><img src={md} alt="Markdown" /><br /><hr />Writting</div>
+                <div className="skills-figma" id="skill"><img src={figma} alt="Figma" /><br /><hr />Design</div>
+                <div className="skills-vercel" id="skill"><img src={vercel} alt="Vercel" /><br /><hr />Deployment</div>
                 <p className="lang">Tools</p>
             </div>
         </div>
