6578e801507cf3e3be6e80116b0686648eaf887e
Rohan-Shridhar <rohansm668@gmail.com>
2026-04-04 12:02:31 +0530

    Added 2 new projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,6 @@
 const { useState, useEffect } = React;
-const project1 = "images/black-loginpage.png";
-const project2 = "images/prisioner-delima.png";
+const project1 = "images/todolist.png";
+const project2 = "images/notjs.png";
 const project3 = "images/grid.png";
 const Projects = () => {
 
@@ -22,26 +22,26 @@ const Projects = () => {
                 </div>
                 <div className="project-card">
                     <div className="project-card-img">
-                        <img src={project2} alt="Prisoners Delima" />
+                        <img src={project2} alt="NotJS" />
                     </div>
-                    <div className="project-card-title">Prisoners Delima</div>
-                    <div className="project-card-desc">The Prisoner's Dilemma is a classic thought experiment in game theory, a field that studies how and why people make decisions when their outcomes depend on the choices of others.</div>
+                    <div className="project-card-title">NotJS</div>
+                    <div className="project-card-desc">A simple code recognition application for only javascript and throws 418 teapot HTTp error if javascript is not detected</div>
                     <div className="project-card-btn">
                         <div />
-                        <a href="https://github.com/Rohan-Shridhar/Prisoners-Delima"><i className="fab fa-github"></i></a>
-                        <a href="https://rohan-shridhar.github.io/Prisoners-Delima/"><i className="fas fa-globe"></i></a>
+                        <a href="https://github.com/Rohan-Shridhar/NotJS"><i className="fab fa-github"></i></a>
+                        <a href="https://rohan-shridhar.github.io/NotJS/"><i className="fas fa-globe"></i></a>
                     </div>
                 </div>
                 <div className="project-card">
                     <div className="project-card-img">
-                        <img src={project1} alt="Attendance Tracker" />
+                        <img src={project1} alt="To Do List" />
                     </div>
-                    <div className="project-card-title">Attendance Tracker</div>
-                    <div className="project-card-desc">A web application that tracks attendance of students and lets teachers mark attendance of students.</div>
+                    <div className="project-card-title">To Do List</div>
+                    <div className="project-card-desc">A basic, functional To-Do List application built with React for managing tasks. Includes basic task management functionality.</div>
                     <div className="project-card-btn">
                         <div />
-                        <a href="https://github.com/Rohan-Shridhar/Attendance-Tracker"><i className="fab fa-github"></i></a>
-                        <a href="https://attendance-tracker-seven-kappa.vercel.app/"><i className="fas fa-globe"></i></a>
+                        <a href="https://github.com/Rohan-Shridhar/To-Do-List"><i className="fab fa-github"></i></a>
+                        <a href="https://to-do-list-iota-fawn-49.vercel.app"><i className="fas fa-globe"></i></a>
                     </div>
                 </div>
             </div>
