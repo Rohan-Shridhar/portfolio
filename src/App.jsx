248d3ebaed2e8fc79cbd6e83faa788a7f5e8294c
Rohan S Mirjankar <rohansm668@gmail.com>
2026-06-24 12:58:00 +0530

    Create main and App jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-const App = () => {
-    return (<>
-        <section className="menu"><Menu /></section>
+import { useState } from 'react'
+import Menu from './Menu.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Skills from './Skills.jsx'
+import Contact from './Contact.jsx'
+import Projects from './Projects.jsx'
+import Footer from './Footer.jsx'
 
+function App() {
+
+  return (<>
+        <section className="menu"><Menu /></section>
         <section className="panel"><Home /></section>
         <section className="panel"><About /></section>
         <section className="panel"><Skills /></section>
         <section className="panel"><Contact /></section>
         <section className="panel"><Projects /></section>
-
         <section className="panel"><Footer /></section>
-    </>);
+    </>
+  );
 }
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-root.render(<App />);
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
