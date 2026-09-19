180ff9c74a5315a0d18dc68efb783eee7e584d37
Rohan S Mirjankar <rohansm668@gmail.com>
2026-09-19 10:47:33 +0530

    feat: add @vercel/analytics dependency and initialize in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { applyEnvironment, getInitialEnvironment } from './theme.js'
-
+import { inject } from '@vercel/analytics';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+inject();
 applyEnvironment(getInitialEnvironment());
 
 createRoot(document.getElementById('root')).render(
